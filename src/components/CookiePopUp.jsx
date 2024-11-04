6ecf56f5f5a5af9a431f9e66b6b6c1b13915661a
Jules KJ <juljen2730@gmail.com>
2024-11-04 14:41:18 +0100

    tailwind made

--- src/components/CookiePopUp.jsx
+++ src/components/CookiePopUp.jsx
@@ -1,14 +1,15 @@
 import Button from "@/components/Button";
 import { FaCookieBite } from "react-icons/fa";
 
-const CookiePopUp = ({ children }) => {
+const CookiePopUp = () => {
   return (
-    <div className="cookie_container">
+    <div className="cookie_container flex flex-col gap-8 bg">
       <FaCookieBite className="cookieBite" />
-
-      <p>we use cookies to improve your experience</p>
+      <p className="capitalize">we use cookies to improve your experience</p>
       <div className="btn_container">
-        <Button btnText="i like cookies"></Button>
+        <button className="bg-black py-2 text-white outline outline-black hover:outline-offset-2 rounded-full capitalize">
+          i would like cookies
+        </button>
       </div>
     </div>
   );
